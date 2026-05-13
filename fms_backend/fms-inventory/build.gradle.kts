233bde0b9fb89e8718c6dc2085cbd165bede42d3
minhvuong3322 <vuongthm4994@ut.edu.vn>
2026-05-13 21:11:08 +0700

    cập nhật

--- fms_backend/fms-inventory/build.gradle.kts
+++ fms_backend/fms-inventory/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 
 dependencies {
     implementation(project(":fms-common"))
+    implementation(project(":fms-user"))
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
 }
