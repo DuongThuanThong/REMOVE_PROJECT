1ec255b839c8c113ffecd77e144314c3ae2e0017
Manh_Dev <manhnguyen0865280164@gmail.com>
2026-05-13 11:10:38 +0700

    create entity (fms-production, fms-customer: Customer) (#10)

---
+++ fms_backend/fms-production/src/test/java/com/uth/fms/production/entity/TaskAssignmentLog.java
@@ -0,0 +1,38 @@
+package com.uth.fms.production.entity;
+
+import com.uth.fms.common.entity.BaseEntity;
+// import com.uth.fms.user.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Table(name = "task_assignment_logs")
+public class TaskAssignmentLog extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "task_id", nullable = false)
+    private ProductionTask task;
+
+    // @ManyToOne(fetch = FetchType.LAZY)
+    // @JoinColumn(name = "assigned_to", nullable = false)
+    // private User assignedTo;
+
+    // @ManyToOne(fetch = FetchType.LAZY)
+    // @JoinColumn(name = "assigned_by", nullable = false)
+    // private User assignedBy;
+
+    @Column(columnDefinition = "text")
+    private String reason;
+
+    @Column(name = "assigned_at", nullable = false, updatable = false)
+    private LocalDateTime assignedAt;
+}
